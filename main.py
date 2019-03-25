d17bcecc0a65cb695ac3fdde44e28e5354cdd612
Vitor Barros <vba@cesar.org.br>
2019-03-25 10:57:39 -0300

    Initial main daemon app with glib mainloop
    Signed-off-by: Vitor Barros <vba@cesar.org.br>

---
+++ main.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2019, CESAR. All rights reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+import sys
+import signal
+import lockfile
+import functools
+import logging
+import dbus
+import dbus.service
+import dbus.mainloop.glib
+from gi.repository import GObject
+
+import daemon
+
+mainloop = None
+
+logging.basicConfig(format='[%(levelname)s] %(funcName)s: %(message)s\n',
+                    stream=sys.stderr, level=logging.INFO)
+
+
+def quit_cb(signal_number, stack_frame):
+    logging.info("Terminate with signal %d" % signal_number)
+    mainloop.quit()
+
+
+context = daemon.DaemonContext(
+    working_directory='/home/vitorbarros/newLora/knot-gatt-service',
+    umask=0o002,
+    detach_process=False,
+    pidfile=lockfile.FileLock(
+        '/home/vitorbarros/newLora/knot-gatt-service/pid.pid'),
+    signal_map={signal.SIGTERM: quit_cb, signal.SIGINT: quit_cb},
+    stdout=sys.stdout,
+    stderr=sys.stderr,
+)
+
+
+with context:
+    dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+
+    mainloop = GObject.MainLoop()
+
+    mainloop.run()
