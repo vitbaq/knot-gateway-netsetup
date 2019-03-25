953a201cc5fc5bae6dd1dcefc89688b4076ebf78
Vitor Barros <vba@cesar.org.br>
2019-03-25 11:00:58 -0300

    Integrate daemon with Wpantund
    This patch integrates the main application with wpantund to get
    openthread information (network name, channel, pan id, xpan id, and
    masterkey). The integration is done through DBus API.
    
    Two methods are used to get these properties: Status and GetProp.
    The second one is used to get the masterkey and the first one get the
    others properties.
    
    Signed-off-by: Vitor Barros <vba@cesar.org.br>

---
+++ wpantun.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2019, CESAR. All rights reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+import sys
+import logging
+import dbus
+import dbus.mainloop.glib
+import gobject as GObject
+
+logging.basicConfig(format='[%(levelname)s] %(funcName)s: %(message)s\n',
+                    stream=sys.stderr, level=logging.INFO)
+
+INTERFACE_SERVICE_DBUS = "com.nestlabs.WPANTunnelDriver"
+INTERFACE_DBUS = "org.wpantund.v1"
+INTERFACE_DBUS_PATH = "/org/wpantund/wpan0"
+
+
+class Singleton(type):
+    """
+    Singleton class to guarantee that a single instance will be used for
+    its inhereted classes
+    """
+    __instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls.__instances:
+            cls.__instances[cls] = super(Singleton,
+                                         cls).__call__(*args, **kwargs)
+        return cls.__instances[cls]
+
+
+class Wpantun(object):
+    __metaclass__ = Singleton
+
+    bus = None
+    iface = None
+    iface_state = None
+
+    state = ""
+    node_type = ""
+    network_name = ""
+    pan_id = 0
+    channel = 0
+    xpan_id = ""
+    mesh_ipv6 = ""
+    masterkey = ""
+
+    def __init__(self):
+        self.bus = dbus.SystemBus()
+        self.iface = self.bus.get_object(
+            INTERFACE_SERVICE_DBUS,
+            INTERFACE_DBUS_PATH)
+
+        self.iface_state = self.bus.get_object(
+            INTERFACE_SERVICE_DBUS,
+            "/com/nestlabs/WPANTunnelDriver/wpan0/Properties/NCP/State")
+
+        self.refresh_values()
+        self._register_signals_listener()
+
+    def _register_signals_listener(self):
+        self.iface_state.connect_to_signal(
+            "PropertyChanged", self.refresh_values())
+
+    def refresh_values(self):
+        status = self.iface.Status(dbus_interface=INTERFACE_DBUS)
+
+        self.state = status.get("NCP:State")
+
+        if self.state == "associated":
+            self.node_type = status.get("Network:NodeType")
+            self.network_name = status.get("Network:Name")
+            self.pan_id = status.get("Network:PANID")
+            self.channel = status.get("NCP:Channel")
+            self.xpan_id = status.get("Network:XPANID")
+            self.mesh_ipv6 = status.get("IPv6:MeshLocalAddress")
+
+            mkey = self.iface.PropGet(
+                "Network:Key", dbus_interface=INTERFACE_DBUS)[1]
+
+            self.masterkey = ":".join(['%02x' % item for item in mkey])
