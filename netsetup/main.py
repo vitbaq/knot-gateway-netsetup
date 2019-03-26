2c7d7eb7b950f7dc7b58531c1627942168fee2e1
Vitor Barros <vba@cesar.org.br>
2019-03-26 17:51:56 -0300

    main: Include argument parser
    This patch include argument parser to get working directory, pid file
    path and detach options from user, if needed.
    
    Signed-off-by: Vitor Barros <vba@cesar.org.br>

--- netsetup/main.py
+++ netsetup/main.py
@@ -10,6 +10,7 @@ import signal
 import lockfile
 import functools
 import logging
+import argparse
 import dbus
 import dbus.service
 import dbus.mainloop.glib
@@ -48,12 +49,23 @@ def register_gatt_error_cb(error):
     mainloop.quit()
 
 
+parser = argparse.ArgumentParser(description="KNoT NetSetup Daemon")
+parser.add_argument("-w", "--working-dir", metavar="<path>",
+                    default="/usr/local/bin/", type=str,
+                    help="Daemon working directory")
+parser.add_argument("-p", "--pid-file", metavar="<path/pid.pid>",
+                    default="/etc/knot/pid.pid", type=str,
+                    help="PID file path and name")
+parser.add_argument("-n", "--detach-process", action="store_false",
+                    help="Detached process")
+args = parser.parse_args()
+
+
 context = daemon.DaemonContext(
-    working_directory='/home/vitorbarros/newLora/knot-gatt-service',
+    working_directory=args.working_dir,
     umask=0o002,
-    detach_process=False,
-    pidfile=lockfile.FileLock(
-        '/home/vitorbarros/newLora/knot-gatt-service/pid.pid'),
+    detach_process=args.detach_process,
+    pidfile=lockfile.FileLock(args.pid_file),
     signal_map={signal.SIGTERM: quit_cb, signal.SIGINT: quit_cb},
     stdout=sys.stdout,
     stderr=sys.stderr,
