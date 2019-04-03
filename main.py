d8f227f918b0608beb8d19667f3a8785f6b09e74
Vitor Barros <vba@cesar.org.br>
2019-04-03 15:27:55 -0300

    main: Add main function
    Signed-off-by: Vitor Barros <vba@cesar.org.br>

--- main.py
+++ main.py
@@ -48,38 +48,44 @@ def register_gatt_error_cb(error):
     mainloop.quit()
 
 
-context = daemon.DaemonContext(
-    working_directory='/usr/local/bin',
-    umask=0o002,
-    detach_process=False,
-    pidfile=lockfile.FileLock('/tmp/netsetup'),
-    signal_map={signal.SIGTERM: quit_cb, signal.SIGINT: quit_cb},
-    stdout=sys.stdout,
-    stderr=sys.stderr,
-)
+def main():
+    global mainloop
+
+    context = daemon.DaemonContext(
+        working_directory='/usr/local/bin',
+        umask=0o002,
+        detach_process=False,
+        pidfile=lockfile.FileLock('/tmp/netsetup'),
+        signal_map={signal.SIGTERM: quit_cb, signal.SIGINT: quit_cb},
+        stdout=sys.stdout,
+        stderr=sys.stderr,
+    )
+
+    with context:
+        dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+
+        wpan = Wpantun()
+        bluetooth = Ble(wpan)
+
+        mainloop = GObject.MainLoop()
+
+        bluetooth.ad_manager.RegisterAdvertisement(
+            bluetooth.ad_knot.get_path(), {},
+            reply_handler=register_ad_reply_cb,
+            error_handler=register_ad_error_cb)
+
+        bluetooth.gatt_manager.RegisterApplication(
+            bluetooth.gatt_knot.get_path(), {},
+            reply_handler=register_gatt_reply_cb,
+            error_handler=register_gatt_error_cb)
+
+        mainloop.run()
+
+        bluetooth.gatt_manager.UnregisterApplication(bluetooth.gatt_knot)
+        bluetooth.ad_manager.UnregisterAdvertisement(bluetooth.ad_knot)
+        dbus.service.Object.remove_from_connection(bluetooth.gatt_knot)
+        dbus.service.Object.remove_from_connection(bluetooth.ad_knot)
 
 
-with context:
-    dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
-
-    wpan = Wpantun()
-    bluetooth = Ble(wpan)
-
-    mainloop = GObject.MainLoop()
-
-    bluetooth.ad_manager.RegisterAdvertisement(
-        bluetooth.ad_knot.get_path(), {},
-        reply_handler=register_ad_reply_cb,
-        error_handler=register_ad_error_cb)
-
-    bluetooth.gatt_manager.RegisterApplication(
-        bluetooth.gatt_knot.get_path(), {},
-        reply_handler=register_gatt_reply_cb,
-        error_handler=register_gatt_error_cb)
-
-    mainloop.run()
-
-    bluetooth.gatt_manager.UnregisterApplication(bluetooth.gatt_knot)
-    bluetooth.ad_manager.UnregisterAdvertisement(bluetooth.ad_knot)
-    dbus.service.Object.remove_from_connection(bluetooth.gatt_knot)
-    dbus.service.Object.remove_from_connection(bluetooth.ad_knot)
+if __name__ == "__main__":
+    main()
