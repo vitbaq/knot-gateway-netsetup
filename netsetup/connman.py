762520c57c49640bb4fee282638d82dd8470b30f
Ramon Ribeiro <rhpr@cesar.org.br>
2019-07-28 21:35:00 -0300

    connman: Register wifi agent to allow connection
    This patch registers a connman agent to send the password to a wifi
    network when it is requested to the user.
    
    For more information about connman agent API see the following link:
    https://git.kernel.org/pub/scm/network/connman/connman.git/tree/doc/agent-api.txt
    
    Signed-off-by: Ramon Ribeiro <rhpr@cesar.org.br>

--- netsetup/connman.py
+++ netsetup/connman.py
@@ -10,6 +10,32 @@ from dbus.exceptions import DBusException
 
 CONNMAN_SERVICE_NAME = 'net.connman'
 CONNMAN_MANAGER_INTERFACE = '%s.Manager' % CONNMAN_SERVICE_NAME
+CONNMAN_AGENT_INTERFACE = '%s.Agent' % CONNMAN_SERVICE_NAME
+
+WIFI_AGENT_PATH = '/knot/netsetup/wifi/agent'
+
+
+class WifiAgent(dbus.service.Object):
+    @dbus.service.method(CONNMAN_AGENT_INTERFACE,
+                         in_signature='', out_signature='')
+    def Release(self):
+        logging.info('release')
+
+    @dbus.service.method(CONNMAN_AGENT_INTERFACE,
+                         in_signature='', out_signature='')
+    def Cancel(self):
+        logging.info('cancel')
+
+    @dbus.service.method(CONNMAN_AGENT_INTERFACE,
+                         in_signature='oa{sv}',
+                         out_signature='a{sv}')
+    def RequestInput(self, path, fields):
+        response = {}
+        if 'Passphrase' in fields:
+            response.update({'Passphrase': self.passphrase})
+
+        logging.info('returning %s' % str(response))
+        return response
 
 
 class ConnmanClient(object):
@@ -17,6 +43,8 @@ class ConnmanClient(object):
         self.bus = dbus.SystemBus()
 
         self.manager = self.__get_manager_interface()
+        self.agent = WifiAgent(self.bus, WIFI_AGENT_PATH)
+        self.manager.RegisterAgent(WIFI_AGENT_PATH)
 
     def __get_manager_interface(self):
         try:
