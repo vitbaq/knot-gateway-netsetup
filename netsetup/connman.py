44892ebb8c1965a5ac3fd6839185d7f9a2418953
Ramon Ribeiro <rhpr@cesar.org.br>
2019-07-28 21:45:23 -0300

    connman: Add scan_wifi method
    This patch adds the Scan method to connman client. As described by connman
    technology API, the Scan is called by an timeout to avoid the blocking
    the mainloop. And a signal ServicesChanged is emitted when the scan is
    finished.
    
    See:
    https://git.kernel.org/pub/scm/network/connman/connman.git/tree/doc/technology-api.txt
    
    Signed-off-by: Ramon Ribeiro <rhpr@cesar.org.br>

--- netsetup/connman.py
+++ netsetup/connman.py
@@ -10,6 +10,7 @@ from dbus.exceptions import DBusException
 
 CONNMAN_SERVICE_NAME = 'net.connman'
 CONNMAN_MANAGER_INTERFACE = '%s.Manager' % CONNMAN_SERVICE_NAME
+CONNMAN_TECHNOLOGY_INTERFACE = '%s.Technology' % CONNMAN_SERVICE_NAME
 CONNMAN_AGENT_INTERFACE = '%s.Agent' % CONNMAN_SERVICE_NAME
 
 WIFI_AGENT_PATH = '/knot/netsetup/wifi/agent'
@@ -43,6 +44,11 @@ class ConnmanClient(object):
         self.bus = dbus.SystemBus()
 
         self.manager = self.__get_manager_interface()
+        if not self.manager:
+            logging.error('Unable to get connman manager. Probably \
+                Connman is not running')
+            return
+
         self.agent = WifiAgent(self.bus, WIFI_AGENT_PATH)
         self.manager.RegisterAgent(WIFI_AGENT_PATH)
 
@@ -55,3 +61,52 @@ class ConnmanClient(object):
             logging.error('%s: DBus Error, Connman is probably not \
                 running.' % err)
             return
+
+    def __enable_wifi(self, iface):
+        properties = iface.GetProperties()
+        if not properties.get('Powered'):
+            iface.SetProperty('Powered', dbus.Boolean(True))
+            logging.info('Enabling wifi')
+
+    def __get_wifi_technology(self):
+        if not self.manager:
+            logging.error('Unable to get wifi technology. Connman not running')
+            return
+
+        for obj_path, properties in self.manager.GetTechnologies():
+                if properties.get('Type') == 'wifi':
+                        break
+
+        if properties.get('Type') != 'wifi':
+            logging.error('Not found technology wifi')
+            return
+
+        wifi_tech_iface = dbus.Interface(
+            self.bus.get_object(CONNMAN_SERVICE_NAME, obj_path),
+            CONNMAN_TECHNOLOGY_INTERFACE)
+
+        return wifi_tech_iface
+
+    def __scan_wifi(self, on_scan_completed):
+        if not self.manager:
+            logging.error('Unable to connect to wifi service')
+            return []
+
+        wifi_tech = self.__get_wifi_technology()
+        if not wifi_tech:
+            logging.error('Not found technology wifi')
+            return
+
+        services = []
+
+        self.__enable_wifi(wifi_tech)
+
+        def on_services_changed(currentServices, rmServices):
+            self.signal_match.remove()
+            on_scan_completed(currentServices)
+
+        self.signal_match = self.manager.connect_to_signal(
+            'ServicesChanged', on_services_changed)
+
+        wifi_tech.Scan(reply_handler=lambda: logging.info('Scan complete'),
+                       error_handler=lambda err: logging.error(err))
